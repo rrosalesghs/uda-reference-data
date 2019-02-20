641c81959d863ceab0bc3ee627a6365d6fdfa8a1
rrosales <rrosales@geisinger.edu>
2019-02-20 16:05:13 -0500

    First commit

---
+++ src/main/java/edu/geisinger/uda/udareferencedata/UdaReferenceDataApplication.java
@@ -0,0 +1,13 @@
+package edu.geisinger.uda.udareferencedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdaReferenceDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdaReferenceDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/geisinger/uda/udareferencedata/UdaReferenceDataApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.geisinger.uda.udareferencedata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdaReferenceDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
